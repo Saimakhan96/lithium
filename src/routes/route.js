53763a1d02708de9028261f0feb4290516e8e97a
Saimakhan96 <saimasadaf.khan96@gmail.com>
2022-11-07 21:20:55 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const nodemon = require('nodemon')
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -14,65 +15,6 @@ router.get('/students', function (req, res){
     res.send(students)
 })
 
-router.get('/movies',function (req,res){
-    console.log("The list of movies :",req.params)
-    let movies=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    res.send(movies)
-})
-
- router.get('/films',function (req,res){
-    console.log("The Array of films is:",req.params)
-   
-
-    let films=[{
-        "id":1,
-        "name":"The Shining"
-    },
-    {
-        "id":2,
-        "name":"Incendies"
-    },
-    {
-        "id":3,
-        "name":"Rang de Basanti"
-    }]
-    
-   
-    res.send(films)
-    
-})
-router.get('/films/:idNumber',function (req,res){
-    let films=[{
-        "id":1,
-        "name":"The Shining"
-    },
-    {
-        "id":2,
-        "name":"Incendies"
-    },
-    {
-        "id":3,
-        "name":"Rang de Basanti"
-    }]
-    let myParams2=req.params.idNumber
-   if(myParams2>films.length){
-    console.log("Invalid Id")
-    res.send("No movie exists with this id")
-   }
-})
-
- 
-
-
-router.get('/movies/:indexNumber',function (req,res){
-    let moviesName=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-   let myParams1=req.params.indexNumber
-   console.log("The path params in the request are :",myParams1)
-   
-   res.send("The movie at the first index is "+ moviesName[indexNumber] )
-
-})
-
 
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
@@ -83,7 +25,6 @@ router.get('/students/:studentName', function(req, res){
     // key is the variable in the route
     // value is whatever dynamic value sent in the request url
     let myParams = req.params
-
 
     // params attribute is fixed in a request object
     // params contains the path parameters object
@@ -99,5 +40,59 @@ router.get('/student-details/:name', function(req, res){
     console.log('Name of the student is ', studentName)
     res.send('Dummy response')
 })
+
+//problem 1
+router.get('/movies', function(req, res){
+    const bollywoodmovies=['ddlj','bodyguard','super30','tiger']
+    console.log("List of movies:",bollywoodmovies)
+
+})
+// problem2
+
+router.get('/movies/:indexNumber',function (req,res){
+    let moviesName=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
+   let myParams1=req.params.indexNumber
+   console.log("The path params in the request are :",myParams1)
+   res.send("The movie at the first index is "+ moviesName[myParams1] )
+   })
+
+ //problem3   
+router.get('/movie/:indexNumber', function(req, res){
+    const movies= [{ "id":1,
+                     "name":'Rang de basanti'
+                    },
+                    {"id":2,
+                    "name": 'The shining'
+                    },
+                    {"id":3,
+                    "name": "Lord of the rings"
+                    },
+                    {"id": 4,
+                    "name": 'Batman begins'
+                    }]
+
+    let requestParams = req.params.indexNumber
+    if(requestParams>movies.length)
+    console.log("Invalid Id")
+    res.send("No movie exists with this id")
+ })
+ //problem4
+
+ router.get('/films', function(req, res){
+    const movies= [{ "id":1,
+                     "name":'Rang de basanti'
+                    },
+                    {"id":2,
+                    "name": 'The shining'
+                    },
+                    {"id":3,
+                    "name": "Lord of the rings"
+                    },
+                    {"id": 4,
+                    "name": 'Batman begins'
+                    }]
+    res.send(movies)
+            })
+
 
 module.exports = router;
